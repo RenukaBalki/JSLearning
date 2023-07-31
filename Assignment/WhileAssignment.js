ea40ac98f98464fc7956a5375bbf3231540ef574
RenukaBalki <renukapbalki@gmail.com>
2023-07-31 13:12:11 +0530

    for loop and while loop
    for loop and while loop

---
+++ Assignment/WhileAssignment.js
@@ -0,0 +1,50 @@
+console.log(`____Step 1 5 to 15_______`);
+var index=5;
+while (index<=15) 
+{
+    console.log(index);
+    index++;
+}
+console.log(`____Step 2 reverse number 50 to 40_______`);
+index=50;
+while (index>=40) 
+{
+    console.log(index);
+    index--;
+}
+console.log(`____Step 3 Odd number_______`);
+index=1;
+while (index<=30) 
+{
+    console.log(index);
+    index=index+2;
+}
+console.log(`____Step 4 Even number_______`);
+index=0;
+while (index<=20) 
+{
+    console.log(index);
+    index=index+2;
+}
+console.log(`____Step 5 -5 table_______`);
+index=5;
+while (index<=50) 
+{
+    console.log(index);
+    index=index+5;
+}
+console.log(`____Step 6 -10 table_______`);
+index=10;
+while (index<=100) 
+{
+    console.log(index);
+    index=index+10;
+}
+console.log(`____Step 7 -10 table Reverse_______`);
+
+index=100;
+while (index>=10) 
+{
+    console.log(index);
+    index=index-10;
+}
---
+++ Assignment/loopForAssignment.js
@@ -0,0 +1,47 @@
+console.log(`____Step 1 5 to 15_______`);
+for (let index = 5; index <=15 ; index++) 
+{
+   console.log(index);
+}
+console.log(`____Step 2 reverse number 50 to 40_______`);
+
+for (let index = 50; index>=40 ; index--) 
+{
+   console.log(index);
+}
+console.log(`____Step 3 Odd number_______`);
+
+for (let index = 1; index<=30; index=index+2) 
+{
+   console.log(index);
+}
+console.log(`____Step 4 Even number_______`);
+
+for (let index = 0; index<=20; index=index+2) 
+{
+   console.log(index);
+}
+console.log(`____Step 4 Even number_______`);
+
+for (let index = 0; index<=20; index=index+2) 
+{
+   console.log(index);
+}
+console.log(`____Step 5 -5 table_______`);
+
+for (let index = 5; index<=50; index=index+5) 
+{
+   console.log(index);
+}
+console.log(`____Step 6 -10 table_______`);
+
+for (let index = 10; index<=100; index=index+10) 
+{
+   console.log(index);
+}
+console.log(`____Step 7 -10 table Reverse_______`);
+
+for (let index = 100; index>=10; index=index-10) 
+{
+   console.log(index);
+}
