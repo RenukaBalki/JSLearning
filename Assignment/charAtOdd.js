53008c5588575cb1e33dcbac6a5f1e61ba49abe1
RenukaBalki <renukapbalki@gmail.com>
2023-08-01 09:09:07 +0530

    scope logical programs
    scope logical programs

---
+++ Assignment/charAtOdd.js
@@ -0,0 +1,9 @@
+function oddPositionedChars(str) {
+    for (let index = 0; index < str.length; index++) {
+        const char = str.charAt(index);
+        
+    }
+
+}
+oddPositionedChars("hello");
+oddPositionedChars("test");
